e69c8b3012ca38d5009b8d4dbbccef5275334758
Sanchi Deshmukh <deshmukhsanchi2400@gmail.com>
2025-08-08 20:41:16 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // <-- proxy all /api calls to your backend running on 5000
+      '/api': 'http://localhost:5000', // Proxy API request to the backend server
     },
   },
 })
